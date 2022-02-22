593c955049903bb9a5254bb317ce864500f7b912
abbos1999 <abbosfayziboev@gmail.com>
2022-02-22 15:51:12 +0500

    setting_bottom_nav_correcting

---
+++ app/src/main/java/com/mac/allomalar/ui/AllomalarActivity.kt
@@ -0,0 +1,28 @@
+package com.mac.allomalar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mac.allomalar.R
+import com.mac.allomalar.databinding.ActivityAllomalarBinding
+import kotlin.concurrent.fixedRateTimer
+
+class AllomalarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAllomalarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAllomalarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavGraph()
+    }
+
+    private fun setBottomNavGraph() {
+       val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.fragmentContainerView)
+        bottomNav.setupWithNavController(navController)
+    }
+}
