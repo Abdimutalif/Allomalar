07befe8da6c0e740f9d974a2e09d55a217abf25f
Abdimutalif <abdimutalifnormengliyev@gmail.com>
2022-02-22 07:34:22 +0300

    Initial commit

---
+++ app/src/main/java/com/mac/allomalar/adapters/PagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.mac.allomalar.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(fm: FragmentManager, behavior: Int) : FragmentPagerAdapter(fm, behavior) {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
