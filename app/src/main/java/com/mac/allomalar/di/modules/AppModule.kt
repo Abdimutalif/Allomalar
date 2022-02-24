4100dd539952d58074914e215b9ec932e92428c9
abbos1999 <abbosfayziboev@gmail.com>
2022-02-24 10:38:18 +0500

    commit_module

---
+++ app/src/main/java/com/mac/allomalar/di/modules/AppModule.kt
@@ -0,0 +1,14 @@
+package com.mac.allomalar.di.modules
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+//
+//    @Provides
+//    fun provideRetrofit()
+}
--- app/src/main/java/com/mac/allomalar/ui/avtivoties/AllomalarActivity.kt
+++ app/src/main/java/com/mac/allomalar/ui/avtivoties/AllomalarActivity.kt
@@ -1,4 +1,4 @@
-package com.mac.allomalar.ui
+package com.mac.allomalar.ui.avtivoties
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
