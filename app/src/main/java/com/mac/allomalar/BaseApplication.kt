843a0f28983832597a8c51eedee9d630358f0ae0
Abdimutalif <abdimutalifnormengliyev@gmail.com>
2022-02-24 23:13:18 +0300

    Alloma.kt AllomaDao.kt Allomas.kt AllomasDao.kt

--- app/src/main/java/com/mac/allomalar/BaseApplication.kt
+++ app/src/main/java/com/mac/allomalar/BaseApplication.kt
@@ -1,8 +1,20 @@
 package com.mac.allomalar
 
+import android.app.AppComponentFactory
 import android.app.Application
+import com.mac.allomalar.di.modules.AppComponent
+import com.mac.allomalar.ui.avtivoties.AllomalarActivity
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Singleton
 
 @HiltAndroidApp
 class BaseApplication : Application(){
+    companion object{
+        lateinit var appComponent: AppComponent
+    }
+    @Singleton
+    override fun onCreate() {
+        super.onCreate()
+
+    }
 }
