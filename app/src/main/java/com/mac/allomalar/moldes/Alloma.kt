0462723ce60540180cf80208f634372bc7476d9b
abbos1999 <abbosfayziboev@gmail.com>
2022-02-24 11:19:32 +0500

    commit_model

---
+++ app/src/main/java/com/mac/allomalar/moldes/Alloma.kt
@@ -0,0 +1,12 @@
+package com.mac.allomalar.moldes
+
+data class Alloma(
+    var about: String, // IMOM   BUXORIY   810-YIL   BUXORODA   TAVALLUD   TOPGAN.    UNING   BOBOKALONI BIRINCHILARDAN  BO'LIB  ISLOMNI  QABUL  QILGAN.  5-6  YOSHLARIDAN  QUR'ON VA DINIY  ILMLAR,  XUSUSAN, HADISLARNI O'RGANISHNI BOSHLAGAN.    IMOM    BUXORIY    KUCHLI    XOTIRA    EGASI    BO'LIB,   O'QIGANINI   TEZ O'ZLASHTIRAR   VA CHUQUR TAHLIL QILAR EDI. 16 YOSHGA  TO'LGANIDA,  ZAMONASIDA  MA'LUM  BO'LGAN   HADISLARNI   YODDAN   BILGAN.
+    var allomamenu: List<String>,
+    var birth_area: String, // Buxoroda tug'igan
+    var birth_year: String, // 810 - yil
+    var id: Int, // 2
+    var image: String, // /images/imombuxoriy.jpeg
+    var madrasa_alloma: String, // Abu Hafs al-Kabir IX asr
+    var name: String // Imom Buxoriy
+)
