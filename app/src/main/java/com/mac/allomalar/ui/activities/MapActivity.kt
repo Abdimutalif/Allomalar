14d6e6f8864a00bafb893058ec99f47188dc5de4
Abdimutalif <abdimutalifnormengliyev@gmail.com>
2022-03-02 16:21:40 +0300

    commit_map_today

--- app/src/main/java/com/mac/allomalar/ui/activities/MapActivity.kt
+++ app/src/main/java/com/mac/allomalar/ui/activities/MapActivity.kt
@@ -3,7 +3,10 @@ package com.mac.allomalar.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.BoringLayout.make
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar.make
 import com.mac.allomalar.databinding.ActivityMapBinding
 import com.mac.allomalar.db.database.AppDatabase
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,9 +25,22 @@ class MapActivity : AppCompatActivity() {
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-
-        binding.nextButton.setOnClickListener {
+        binding.buxoroCard.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Buxoro bosildi",Toast.LENGTH_LONG).show()
             startActivity(Intent(this, AllomalarActivity::class.java))
+        }
+        binding.xivaCard.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Xiva bosildi",Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, AllomalarActivity::class.java))
+        }
+        binding.marvCard.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Marv bosildi",Toast.LENGTH_LONG).show()
+        }
+        binding.makkaCard.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Makka bosildi",Toast.LENGTH_LONG).show()
+        }
+        binding.madinaCard.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Madina bosildi",Toast.LENGTH_LONG).show()
         }
     }
 }
