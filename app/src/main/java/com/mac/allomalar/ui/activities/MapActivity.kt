f03721c79a3a138adf03a3b0b812b5e4ef30ffb0
abbos1999 <abbosfayziboev@gmail.com>
2022-03-03 18:59:44 +0500

    commit_put_maps_last

--- app/src/main/java/com/mac/allomalar/ui/activities/MapActivity.kt
+++ app/src/main/java/com/mac/allomalar/ui/activities/MapActivity.kt
@@ -7,9 +7,14 @@ import android.text.BoringLayout.make
 import android.widget.HorizontalScrollView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentPagerAdapter
 import com.google.android.material.snackbar.Snackbar.make
+import com.mac.allomalar.adapters.PagerAdapterMap
 import com.mac.allomalar.databinding.ActivityMapBinding
 import com.mac.allomalar.db.database.AppDatabase
+import com.mac.allomalar.ui.fragments.PageFirstFragment
+import com.mac.allomalar.ui.fragments.PageSecondFragment
+import com.mac.allomalar.ui.fragments.PageThirdFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -18,33 +23,29 @@ import javax.inject.Inject
 class MapActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapBinding
 
-    @Inject
-    lateinit var db: AppDatabase
-    val x=binding.horizontalScroll.getLeft()
-    val y = binding.horizontalScroll.getTop()
-    //binding.horizontalScroll.scrollTo(x,y)
+//    @Inject
+//    lateinit var db: AppDatabase
+//    val x=binding.horizontalScroll.getLeft()
+//    val y = binding.horizontalScroll.getTop()
+//    //binding.horizontalScroll.scrollTo(x,y)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        binding.buxoroCard.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Buxoro bosildi",Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, AllomalarActivity::class.java))
-        }
-        binding.xivaCard.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Xiva bosildi",Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, AllomalarActivity::class.java))
-        }
-        binding.marvCard.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Marv bosildi",Toast.LENGTH_LONG).show()
-        }
-        binding.makkaCard.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Makka bosildi",Toast.LENGTH_LONG).show()
-        }
-        binding.madinaCard.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Madina bosildi",Toast.LENGTH_LONG).show()
-        }
+
+        val page1 = PageFirstFragment()
+        val page2 = PageSecondFragment()
+        val page3 = PageThirdFragment()
+        val adapter = PagerAdapterMap(
+            page1,
+            page2,
+            page3,
+            3,
+            supportFragmentManager,
+            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+        )
+        binding.vpMap.adapter = adapter
     }
 }
